620fbc64651fa42d6cfb011912f2d42f74d9ca40
hdsrinivas@yahoo.com <hdsrnivas@yahoo.com>
2025-06-23 11:56:02 +0530

    Remove all files from GitHub

--- routes/pingRoute.js
+++
@@ -1,9 +0,0 @@
-// routes/pingRoute.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/ping', (req, res) => {
-  res.status(200).send('pong');
-});
-
-module.exports = router;
